defbcedb0b3404b43c4f3abf8c95766f5f9e89a5
helsik <mymail.kulik@gmail.com>
2018-05-15 12:37:08 +0300

    first commit
     On branch master
     Initial commit
     Changes to be committed:
    	new file:   config
    	new file:   task10_12_2.sh

---
+++ task10_12_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+dir_pwd=$(dirname "$0")
+dir_pwd=$(cd "$dir_pwd" && pwd)
+source ${dir_pwd}/config
+# Install docker-ce
+apt-get update
+apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce -y
+# Install docker compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
